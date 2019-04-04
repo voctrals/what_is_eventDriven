1594db078f29f8ad3f549545255c189de6fe3262
liulei <lei.liu@17zuoye.com>
2019-04-04 16:58:32 +0800

    package-info add

---
+++ src/main/java/voctrals/study/eventdriven/package-info.java
@@ -0,0 +1,11 @@
+package voctrals.study.eventdriven;
+
+/*
+
+事件，事件源，监听器
+
+event object：事件对象，事件产生时具体的“事件”，拥有一个被监听的属性，一般作为listener的方法参数使用。
+event source：事件源，接收事件的实体。
+event listener：事件监听器类。当有对应的event object产生时，调用相应的方法进行处理。
+
+*/
